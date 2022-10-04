57b17be919ba9be19e45da2c2196cf0549994f50
mariwi1503 <mariwi1503@gmail.com>
2022-10-04 14:20:29 +0800

    using an env for credential

--- config/index.js
+++ config/index.js
@@ -3,11 +3,11 @@ require('dotenv').config()
 module.exports = {
     port: process.env.PORT || 5000,
     dbConfig: {
-        port: 6883,
-        host: 'containers-us-west-66.railway.app', // process.env.DB_HOST
-        user: 'root', // process.env.DB_USER
-        password: 'pXcbNRX4IZhsjGhhqaAF', // process.env.DB_PASSWORD
-        database: 'railway', // process.env.DB_NAME
+        port: process.env.DB_PORT,
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
     },
     mailConfig: {
         email: process.env.EMAIL,
